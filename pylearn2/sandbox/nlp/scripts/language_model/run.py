df9c9ebe93cf225e06da8750f0f383e1b867ef3e
rahul003 <rahulhuilgol@gmail.com>
2014-05-29 16:22:49 -0400

    ?"

--- pylearn2/sandbox/nlp/scripts/language_model/run.py
+++ pylearn2/sandbox/nlp/scripts/language_model/run.py
@@ -5,6 +5,7 @@ from pylearn2.termination_criteria import EpochCounter
 from pylearn2.testing.skip import skip_if_no_data
 from pylearn2.config import yaml_parse
 
+CUDA_LAUNCH_BLOCKING=1
 theano.config.exception_verbosity = 'high'
 skip_if_no_data()
 with open('/u/huilgolr/pylearn2/pylearn2/sandbox/nlp/scripts/language_model/class_soft.yaml', 'r') as f:
