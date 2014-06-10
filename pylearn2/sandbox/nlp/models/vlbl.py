finally,t hat elusive bug with softmax

pylearn2/sandbox/nlp/models/vlbl.py
@@ -21,43 +21,33 @@ from pylearn2.costs.cost import Cost
 import math
 import theano
 class vLBLSoft(Model):
-   
     def __init__(self, dict_size, dim, context_length, k, irange = 0.1, seed = 22):
-        #dim is the dimensions of the final representation of each word
-
         super(vLBLSoft, self).__init__()
         rng = np.random.RandomState(seed)
         self.rng = rng
         self.context_length = context_length
         self.dim = dim
         self.dict_size = dict_size
-
         C_values = np.asarray(rng.normal(0, math.sqrt(irange),
                                          size=(dim,context_length)),
                               dtype=theano.config.floatX)
         self.C = theano.shared(value=C_values, name='C', borrow=True)
-
         W_context = rng.uniform(-irange, irange, (dict_size, dim))
         W_context = sharedX(W_context,name='W_context')
         W_target = rng.uniform(-irange, irange, (dict_size, dim))
         W_target = sharedX(W_target,name='W_target')
         self.projector_context = MatrixMul(W_context)
         self.projector_target = MatrixMul(W_target)
-        
         self.W_context = W_context
         self.W_target = W_target
-
         self.W_target = W_context
-
         b_values = np.asarray(rng.normal(0, math.sqrt(irange), size=(dict_size,)),
                               dtype=theano.config.floatX)
         self.b = theano.shared(value=b_values, name='b', borrow=True)
-
         self.input_space = IndexSpace(dim = context_length, max_labels = dict_size)
         self.output_space = IndexSpace(dim = 1, max_labels = dict_size)
-
         self.allY = T.as_tensor_variable(np.arange(dict_size,dtype=np.int64).reshape(dict_size,1))
-
+    
     def get_params(self):
         #get W from projector
         rval1 = self.projector_context.get_params()
@@ -66,7 +56,7 @@ class vLBLSoft(Model):
         rval1.extend([self.C, self.b])
         rval1.extend(rval2)
         return rval1
-
+    
     def fprop(self, state_below):
         """
         state_below is r_w?
@@ -75,44 +65,10 @@ class vLBLSoft(Model):
         rval = self.C.dimshuffle('x', 0, 1) * state_below
         rval = rval.sum(axis=2)
         return rval
-
-    def score(self, X, Y):
-        """
-        So takes X which is context.
-        gets r_w which project returns
-        gets q_hat = from calculating fprop which gives us the predicted representation.
-        Then finds score by doing dot product with the target representation
-        """
-
-        X = self.projector_context.project(X)
-        q_h = self.fprop(X)
-
-        q_w = self.projector_target.project(Y)
-        all_q_w = self.projector_target.project(self.allY)
-        
-        swh = (q_w*q_h).sum(axis=1) + self.b[Y].flatten()
-        sallwh = T.dot(q_h,all_q_w.T) + self.b.dimshuffle('x',0)
-        
-        #swh = T.exp(swh)
-        #sallwh = T.exp(sallwh).sum(axis=1)
-
-        #return swh, sallwh
-        return sallwh
-        #10,5
-        # if ndim == 1: 
-        #     #for vector this is the case
-        #         #.reshape((Y.shape[0], self.dim))
-        #     q_w = self.projector.project(Y)
-        #     rval = (q_w * q_h).sum(axis=1) + self.b[Y].flatten()
-        # elif ndim == 2: 
-        #     #q_w = self.projector.project(Y).reshape((Y.shape[0], Y.shape[1], self.dim)).dimshuffle(1, 0, 2)
-        #     #rval = (q_h.dimshuffle('x', 0, 1) + q_w).sum(axis=1) + self.b[Y].flatten()
-        #     rval = (q_h.dimshuffle('x', 0, 1) * q_w).sum(axis=2) + self.b[Y].flatten()
-        #return rval
-
+    
     def get_default_cost(self):
         return Default()
-
+    
     def get_monitoring_data_specs(self):
         """
         Returns data specs requiring both inputs and targets.
@@ -126,7 +82,7 @@ class vLBLSoft(Model):
                                 self.get_output_space()))
         source = (self.get_input_source(), self.get_target_source())
         return (space, source)
-
+    
     def get_monitoring_channels(self, data):
 
         # W_context = self.W_context
@@ -177,20 +133,37 @@ class vLBLSoft(Model):
         rval['nll'] = self.cost_from_X(data)
         rval['perplexity'] = 10 ** (rval['nll']/np.log(10).astype('float32'))
         return rval
-        
+    
+    def score(self, X, Y):
+        X = self.projector_context.project(X)
+        q_h = self.fprop(X)
+        all_q_w = self.projector_target.project(self.allY)
+        sallwh = T.dot(q_h,all_q_w.T) + self.b.dimshuffle('x',0)
+        return sallwh
     def cost_from_X(self, data):
         X, Y = data
-        #s,sallwh = self.score(X,Y)
-        #prob = s/sallwh
-
         s = self.score(X,Y)
         p_w_given_h = T.nnet.softmax(s)
-        
-        #15x1
-        #return -T.mean(T.log(prob))
-        return -T.mean(T.diag(T.log2(p_w_given_h)[T.arange(Y.shape[0]), Y]))
+        return -T.mean(T.diag(T.log(p_w_given_h)[T.arange(Y.shape[0]), Y]))
 
 class vLBL(Model):
+    
+    def score(self, X, Y):
+        X = self.projector_context.project(X)
+        q_h = self.fprop(X)
+        q_w = self.projector_target.project(Y)
+        all_q_w = self.projector_target.project(self.allY)
+        swh = (q_w*q_h).sum(axis=1) + self.b[Y].flatten()
+        sallwh = T.dot(q_h,all_q_w.T) + self.b.dimshuffle('x',0)
+        swh = T.exp(swh)
+        sallwh = T.exp(sallwh).sum(axis=1)
+        return swh, sallwh
+
+    def cost_from_X(self, data):
+        X, Y = data
+        s,sallwh = self.score(X,Y)
+        prob = s/sallwh
+        return -T.mean(T.log(prob))
     
     def __init__(self, dict_size, dim, context_length, k, irange = 0.1, seed = 22):
 	super(vLBL, self).__init__()
@@ -310,51 +283,6 @@ class vLBL(Model):
         rval['perplexity'] = 10 ** (rval['nll']/np.log(10).astype('float32'))
         return rval
 
-    def score(self, X, Y):
-        """
-        So takes X which is context.
-        gets r_w which project returns
-        gets q_hat = from calculating fprop which gives us the predicted representation.
-        Then finds score by doing dot product with the target representation
-        """
-
-        X = self.projector_context.project(X)
-        q_h = self.fprop(X)
-
-        q_w = self.projector_target.project(Y)
-        all_q_w = self.projector_target.project(self.allY)
-        
-        swh = (q_w*q_h).sum(axis=1) + self.b[Y].flatten()
-        sallwh = T.dot(q_h,all_q_w.T) + self.b.dimshuffle('x',0)
-        
-        swh = T.exp(swh)
-        sallwh = T.exp(sallwh).sum(axis=1)
-
-        return swh, sallwh
-        #return sallwh
-        #10,5
-        # if ndim == 1: 
-        #     #for vector this is the case
-        #         #.reshape((Y.shape[0], self.dim))
-        #     q_w = self.projector.project(Y)
-        #     rval = (q_w * q_h).sum(axis=1) + self.b[Y].flatten()
-        # elif ndim == 2: 
-        #     #q_w = self.projector.project(Y).reshape((Y.shape[0], Y.shape[1], self.dim)).dimshuffle(1, 0, 2)
-        #     #rval = (q_h.dimshuffle('x', 0, 1) + q_w).sum(axis=1) + self.b[Y].flatten()
-        #     rval = (q_h.dimshuffle('x', 0, 1) * q_w).sum(axis=2) + self.b[Y].flatten()
-        #return rval
-
-    def cost_from_X(self, data):
-        X, Y = data
-        s,sallwh = self.score(X,Y)
-        prob = s/sallwh
-
-        #s = self.score(X,Y)
-        #p_w_given_h = T.nnet.softmax(s)
-        
-        #15x1
-        return -T.mean(T.log(prob))
-        #return -T.mean(T.diag(T.log2(p_w_given_h)[T.arange(Y.shape[0]), Y]))
 
 class vLBLNCE(vLBL):
     

pylearn2/sandbox/nlp/scripts/testing.py
@@ -71,14 +71,11 @@ swh = (qw*qh).sum(axis=1) + b[y].flatten()
 sallwh = theano.tensor.dot(qh,allqw.T)+b[ally].flatten()
 
 soft = theano.tensor.nnet.softmax(sallwh)
-a= -T.mean(T.log2(soft)[T.arange(y.shape[0]), y])
+probsoft = T.diag(soft[(T.arange(y.shape[0]),y)])
 
 esallwh = T.exp(sallwh)
 eswh = T.exp(swh)
 esallwh = esallwh.sum(axis=1)
-
-#print esallwh.eval().shape
-#print eswh.eval().shape
 
 prob = eswh/esallwh
 